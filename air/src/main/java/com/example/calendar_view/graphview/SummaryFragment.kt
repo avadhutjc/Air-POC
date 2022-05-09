bf669f6f0cd61d73c0ca8cd85f24d3dacad793d5
avadhutj <avadhutjc@gmail.com>
2022-05-09 20:56:03 +0530

    all changes working

--- air/src/main/java/com/example/calendar_view/graphview/SummaryFragment.kt
+++ air/src/main/java/com/example/calendar_view/graphview/SummaryFragment.kt
@@ -1,26 +1,30 @@
-package com.example.calendar_view
+package com.example.calendar_view.graphview
 
 import android.app.Dialog
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import androidx.cardview.widget.CardView
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.calendar_view.graphview.ColorTemplate1
+import com.example.calendar_view.R
+import com.example.calendar_view.SpinnerDoing
+import com.example.calendar_view.Student
+import com.example.calendar_view.StudentAdapter
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener,
+class SummaryFragment : Fragment(R.layout.fragment_summary), View.OnClickListener,
     AdapterView.OnItemSelectedListener {
 
     var imageButton: ImageButton? = null
@@ -29,11 +33,6 @@ class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener,
     var cardView1: CardView? = null
 
     private var arrayAdapter: ArrayAdapter<String>? = null
-
-    private var recyclerView: RecyclerView? = null
-
-    //   private val studentList = mutableListOf<Student>()
-    private val studentList = arrayListOf<Student>()
 
     // variable for our bar chart
     private var barChart: BarChart? = null
@@ -61,26 +60,15 @@ class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener,
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initListeners()
 
         navController = Navigation.findNavController(view)
-        // spinner = findViewById(R.id.spinners_show)
 
         arrayAdapter =
             ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, itemList)
         spinners_show?.adapter = arrayAdapter
         spinners_show?.onItemSelectedListener = this
 
-        // Drop down layout style - we get space between to list
         arrayAdapter!!.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-
-        /*  spinners_show.onItemSelectedListener.run {
-              val intent = Intent(Intent.ACTION_VIEW).setClassName(
-                  "ru.slybeaver.truecalendar", "ru.slybeaver.truecalendar.CalendarActivity"
-              )
-              startActivity(intent)
-          }*/
 
         spinners_show.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
@@ -100,33 +88,6 @@ class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener,
                         intent = Intent(requireContext(), SpinnerDoing::class.java)
                         startActivity(intent)
                     }
-
-                    2 -> {
-                        intent = Intent(Intent.ACTION_VIEW).setClassName(
-                            "ru.slybeaver.truecalendar",
-                            "ru.slybeaver.truecalendar.CalendarActivity"
-                        )
-                        startActivity(intent)
-                    }
-                    3 -> {
-                        intent = Intent(requireContext(), SpinnerDoing::class.java)
-                        startActivity(intent)
-                    }
-                    4 -> {
-                        intent = Intent(Intent.ACTION_VIEW).setClassName(
-                            "ru.slybeaver.truecalendar",
-                            "ru.slybeaver.truecalendar.CalendarActivity"
-                        )
-                        startActivity(intent)
-                    }
-                    5 -> {
-                        intent = Intent(Intent.ACTION_VIEW).setClassName(
-                            "ru.slybeaver.truecalendar",
-                            "ru.slybeaver.truecalendar.CalendarActivity"
-                        )
-                        startActivity(intent)
-                    }
-
                 }
             }
 
@@ -145,9 +106,9 @@ class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener,
             navController.navigate(R.id.action_homeFragment_to_birthAttendantFragment) // -> onclick of btn will open any fragment assign
         }
 
-        imageButton1 = view.findViewById(R.id.people_img_btn_summary) as ImageButton
+        imageButton1 = view.findViewById(R.id.people_img_btn_back_to_home) as ImageButton
         imageButton1!!.setOnClickListener {
-            navController.navigate(R.id.action_homeFragment_to_summaryFragment)
+            navController.navigate(R.id.action_summaryFragment_to_homeFragment)
         }
 
         imageButton = view.findViewById<View>(R.id.switch_img_btn) as ImageButton
@@ -163,15 +124,9 @@ class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener,
             dialog.getWindow()?.setLayout(1300, 1400); //Controlling width and height.
             dialog.show()
 
-            val manageUserTextview = dialog.findViewById<View>(R.id.btn_manage_user) as TextView
-            manageUserTextview.setOnClickListener {
-                navController.navigate(R.id.action_homeFragment_to_manageUserFragment) // -> onclick of btn will open any fragment assign
-                dialog.dismiss()
-            }
-
             val relativeLayout1 = dialog.findViewById<View>(R.id.rel111) as RelativeLayout
             relativeLayout1.setOnClickListener {
-                navController.navigate(R.id.action_homeFragment_to_birthAttendantFragment) // -> onclick of btn will open any fragment assign
+                navController.navigate(R.id.action_summaryFragment_to_homeFragment) // -> onclick of btn will open any fragment assign
                 dialog.dismiss()
             }
 
@@ -189,35 +144,11 @@ class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener,
 
         }
 
-
-/*
-//        button = view.findViewById<Button>(R.id.btn_manage_user)
-//        button?.setOnClickListener {
-//            val dialog = Dialog(this.requireContext())
-//            // Include dialog.xml file
-//            dialog.setContentView(R.layout.dialog)
-//            Log.d("image_set","abc")
-//            navController.navigate(R.id.action_homeFragment_to_windowFragment) // -> onclick of btn will open any fragment assign
-//        }
-
-        //    button = view.findViewById<View>(R.id.btn_manage_user) as Button
-
-//        imageButton!!.setOnClickListener {
-//           // startActivity(Intent(activity, MainActivity2::class.java)) -> onclick of btn will open any activity assign
-//            navController.navigate(R.id.action_homeFragment_to_windowFragment) // -> onclick of btn will open any fragment assign
-//        }
-//        imageButton!!.setOnClickListener {
-//            val intent = Intent(this@HomeFragment, CreatorCreatePostActivity::class.java)
-//            startActivity(intent)
-//        }
-*/
-
         Log.d("check", "AnotherBarActivity is running")
         //    barChart = view.findViewById<BarChart>(R.id.chart_in_home_frag) -> show wrong initialization
-        barChart = view.findViewById(R.id.home_frag_chart) //-> show correct initialization
-        barChart1 =
-            view.findViewById(R.id.home_frag_chart_ev_duration11) //-> show correct initialization
-        barChart2 = view.findViewById(R.id.home_frag_chart_ev_duration2)
+        //barChart = view.findViewById(R.id.home_frag_chart) //-> show correct initialization
+        //barChart1 = view.findViewById(R.id.home_frag_chart_ev_duration11) //-> show correct initialization
+       // barChart2 = view.findViewById(R.id.home_frag_chart_ev_duration2)
         getBarEntries()
         barDataSet = BarDataSet(barEntriesArrayList, "")
         barData = BarData(barDataSet)
@@ -228,33 +159,8 @@ class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener,
         //  barDataSet?.setColors(*ColorTemplate.MATERIAL_COLORS) ->read only colors
         barDataSet?.setColors(*ColorTemplate1.MATERIAL_COLORS1) //read and write only colors
 
-        initViews()
-        buildStudentList()
-//        setRecyclerView()
     }
 
-    private fun setRecyclerView() {
-        val studentAdapter = StudentAdapter(studentList)
-        val linearLayoutManager = LinearLayoutManager(requireContext())
-//        recyclerView!!.layoutManager = linearLayoutManager
-        //      recyclerView!!.adapter = studentAdapter
-
-
-        Log.d("StudentList", studentList?.size.toString())
-    }
-
-    private fun buildStudentList() {
-        val student1 = Student("Practice Session", 22, "11f")
-        studentList.add(student1)
-    }
-
-    private fun initViews() {
-        //   recyclerView = view?.findViewById<RecyclerView>(R.id.recycler_view_home_frag)
-    }
-
-    private fun initListeners() {
-        btn_detail.setOnClickListener(this)
-    }
 
     override fun onClick(p0: View?) {
         when (p0?.id) {
@@ -265,26 +171,8 @@ class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener,
                 )
                 startActivity(intent)
             }
-
-//            R.id.btn_manage_user -> {
-//                navController.navigate(R.id.action_homeFragment_to_manageUserFragment)
-//            }
-//            R.id.switch_img_btn -> {
-//                startActivity(Intent(activity, MainActivity2::class.java))
-//            }
         }
     }
-
-//    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-//        val items: String = parent?.getItemAtPosition(position) as String
-//        Toast.makeText(requireContext(), items, Toast.LENGTH_LONG).show()
-//
-//    }
-//
-//    override fun onNothingSelected(parent: AdapterView<*>?) {
-//        Toast.makeText(requireContext(), "Nothing Select", Toast.LENGTH_SHORT).show()
-//
-//    }
 
     private fun getBarEntries() {
         // creating a new array list
